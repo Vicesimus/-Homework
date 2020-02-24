39e964ca58ab94dd2b0815ea5c04a7546f004549
Vicesimus <hermit1989@gmail.com>
2020-02-24 14:56:18 +0300

    add .\HomeWork1"

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello!";
+            HelloUser();
+            Console.ReadLine();
+        }
+        static void HelloUser()
+        {
+            Console.Write("Введите свое имя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Здравствуй, {0}!", userName);
+        }
+    }
+}
